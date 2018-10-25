a2cc6b19fbb6b189e49edc24cf6f5874eed3448f
Tomofumi Inoue <funaox@gmail.com>
2018-10-26 02:21:38 +0900

    Impl generator

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,18 @@
 import * as functions from 'firebase-functions';
+import * as bwipjs from 'bwip-js';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+export const generate = functions.https.onRequest((request, response) => {
+    console.info(request.path);
+    const text = unescape(request.path.slice(1, -4));
+    bwipjs(request, response, {
+        bcid: 'code39',
+        text: text,
+        includetext: true,
+        paddingwidth: 8,
+        paddingheight: 8,
+        backgroundcolor: 'FFFFFF'
+    });
+});
